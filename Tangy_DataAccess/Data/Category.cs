73ea6693d912026d17bfdf92bbce6a668580c311
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-05-25 12:37:34 +0300

    Removed the data access folder

--- Tangy_DataAccess/Data/Category.cs
+++
@@ -1,8 +0,0 @@
-namespace Tangy_DataAccess.Data;
-
-public class Category
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public DateTime CreatedDate { get; set; }
-}
