5563179feeafbf4a6c615abe9a2df1a8e22965b2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-05-25 15:27:14 +0300

    Added required data annotations

--- Tangy_WebServer/Data/DTOs/CategoryDTO.cs
+++ Tangy_WebServer/Data/DTOs/CategoryDTO.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Tangy_WebServer.Data.DTOs;
 
 public class CategoryDTO
 {
     public int Id { get; set; }
+    [Required]
     public string? Name { get; set; }
 }
