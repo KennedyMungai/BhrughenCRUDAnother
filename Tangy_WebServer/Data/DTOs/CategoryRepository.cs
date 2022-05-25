55711f8b0b737171fbd57c6f45208173526d4606
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-05-25 14:29:38 +0300

    Finished setting up the file

--- Tangy_WebServer/Data/DTOs/CategoryRepository.cs
+++ Tangy_WebServer/Data/DTOs/CategoryRepository.cs
@@ -17,7 +17,7 @@ public class CategoryRepository : ICategoryRepository
     public CategoryDTO Create(CategoryDTO objDTO)
     {
         var obj = _mapper.Map<CategoryDTO, Category>(objDTO);
-        
+        obj.CreatedDate = DateTime.Now;
         var addedObj = _db.Categories.Add(obj);
         _db.SaveChanges();
 
@@ -26,21 +26,46 @@ public class CategoryRepository : ICategoryRepository
 
     public CategoryDTO Update(CategoryDTO objDTO)
     {
+        var objFromDb = _db.Categories.FirstOrDefault(u => u.Id == objDTO.Id);
 
+        if(objFromDb is not null)
+        {
+            objFromDb.Name = objDTO.Name;
+            _db.Categories.Update(objFromDb);
+            _db.SaveChanges();
+            return _mapper.Map<Category, CategoryDTO>(objFromDb);
+        };
+
+        return objDTO;
     }
 
     public int Delete(int id)
     {
+        var obj = _db.Categories.FirstOrDefault(u => u.Id == id);
 
+        if (obj is not null)
+        {
+            _db.Categories.Remove(obj);
+            return _db.SaveChanges();
+        }
+
+        return 0;
     }
 
     public CategoryDTO Get(int id)
     {
+        var obj = _db.Categories.FirstOrDefault(c => c.Id == id);
 
+        if(obj is not null)
+        {
+            return _mapper.Map<Category, CategoryDTO>(obj);
+        }
+
+        return new CategoryDTO();
     }
 
     public IEnumerable<CategoryDTO> GetAll()
     {
-
+        return _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryDTO>>(_db.Categories);
     }
 }
