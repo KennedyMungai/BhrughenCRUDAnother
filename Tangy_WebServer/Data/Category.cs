18b63361a2f9529296653b6506e5eb626a1247ea
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-05-25 13:13:27 +0300

    Removed the DateTime instantiation

--- Tangy_WebServer/Data/Category.cs
+++ Tangy_WebServer/Data/Category.cs
@@ -7,5 +7,5 @@ public class Category
     [Key]
     public int Id { get; set; }
     public string? Name { get; set; }
-    public DateTime CreatedDate { get; set; } = DateTime.Now;
+    public DateTime CreatedDate { get; set; }
 }
